e808492d03f4ffccf6f06f083af708b504fba3b9
baambox5 <baamboxo@gmail.com>
2019-08-06 20:26:58 +0900

    190806 algorithm problem

--- 190805/ex3.py
+++ 190805/ex3.py
@@ -7,10 +7,35 @@ arr = [[9, 20, 2, 18, 11],
 N, M = len(arr), len(arr[0])
 row = 0
 column = 0
-for i in range(N):
-    for rj in range(M-1, -1, -1):
-        for j in range(M):
-            print(arr[row][column])
 
-    row = (row + ((i+1) % 2)) * (-1)
+for j in range(0, M-1):
+    arr[0][j]
+for i in range(0, N-1):
+    arr[i][-1]
+for j in range(0, M-1):
+    arr[-1][-j-1]
+for i in range(0, N-1):
+    arr[-i-1][0]
+for j in range(1, M-2):
+    arr[1][j]
+for i in range(1, N-2):
+    arr[i][-2]
+for j in range(1, M-2):
+    arr[-2][-j-1]
+for i in range(1, N-2):
+    arr[-i-1][1]
+for j in range(2, M-3):
+    arr[2][2]
 
+
+for j in range(0, M):
+    arr[0][j]
+
+for j in range(0, M-1):
+    arr[0][j]
+for i in range(0, N-1):
+    arr[i][-1]
+for j in range(0, M-1):
+    arr[-1][-j-1]
+for i in range(0, N-1):
+    arr[-i-1][-1]
--- 190806/8104.cpp
+++ 190806/8104.cpp
@@ -1,46 +1,42 @@
 #include <stdio.h>
-#include<iostream>
-
-using namespace std;
 
 int main(int argc, char** argv)
 {
 	int test_case;
-	int T;
-	/*
-	   아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
-	   앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-	   //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-	   freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-	   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
-	   freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
-	   단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
-	*/
-	freopen("8104.txt", "r", stdin);
-	cin>>T;
+	int T = 0;
 	
-	for(test_case = 1; test_case <= T; ++test_case)
+	FILE *fp;
+	freopen_s(&fp, "8104.txt", "r", stdin);
+	scanf_s("%d", &T);
+	
+	for (test_case = 1; test_case <= T; ++test_case)
 	{
-        int N;
-        int K;
-        scanf("%d, %d", &N, &K);
-		
-    //     N, K = tuple(map(int, input().split()))
-    // arr = []
-    // for i in range(K):
-    //     arr.append([0]*N)
-    // for i in range(1, N+1):
-    //     for k in range(1, K+1):
-    //         if i % 2:
-    //             arr[k-1][i-1] += k + (i-1) * K
-    //         else:
-    //             arr[-k][i-1] += k + (i-1) * K
-    // res = ''
-    // for i in range(K):
-    //     res += ' ' + str(sum(arr[i]))
-    // print('#{}{}'.format(test_case, res))
-
-
+		int N = 0;
+		int K = 0;
+		scanf_s("%d", &N);
+		scanf_s("%d", &K);
+		int arr[20][10000];
+		for (int i = 0; i < N; i++) {
+			for (int k = 0; k < K; k++) {
+				arr[k][i] = 0;
+			}
+		}
+		for (int i = 1; i < (N + 1); i++) {
+			for (int k = 1; k < (K + 1); k++) {
+				if (i % 2) arr[k - 1][i - 1] = k + (i - 1) * K;
+				else arr[K - k][i - 1] = k + (i - 1) * K;
+			}
+		}
+		int sum = 0;
+		printf("#%d", test_case);
+		for (int i = 0; i < K; i++) {
+			sum = 0;
+			for (int j = 0; j < N; j++) {
+				sum += arr[i][j];
+			}
+			printf(" %d", sum);
+		}
+		printf("\n");
 	}
 	return 0;
 }
