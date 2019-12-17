2a8666371d0eeab610b527e265fef10941557d25
baambox5 <baamboxo@gmail.com>
2019-12-18 00:18:25 +0900

    commit

--- 191216/17070_파이프옮기기_고병권.py
+++ 191216/17070_파이프옮기기_고병권.py
@@ -3,14 +3,13 @@ sys.stdin = open('move_pipe.txt', 'r')
 
 
 def pipe(x, y, state):
-    print(x, y, state, pipe_memo[x][y][state])
-    if pipe_memo[x][y][state]:
-        print(write)
-        return pipe_memo[x][y][state]
     if house[x][y] == '1':
         return 0
+    if x == N - 1 and y == N - 1:
+        pipe_memo[x][y][state] = 1
+    if pipe_memo[x][y][state] >= 0:
+        return pipe_memo[x][y][state]
     sum_value = 0
-    write.append((x, y, state))
     if state == 0:
         if y < N - 1:
             sum_value += pipe(x, y + 1, 0)
@@ -27,19 +26,12 @@ def pipe(x, y, state):
             sum_value += pipe(x + 1, y, 2)
     if x < N - 1 and y < N - 1:
         sum_value += pipe(x + 1, y + 1, 1)
-    if sum_value:
-        pipe_memo[x][y][state] = 1
-    write.pop()
+    pipe_memo[x][y][state] = sum_value
     return sum_value
 
 
 N = int(input())
 house = [input().split() for _ in range(N)]
-pipe_memo = [[[0, 0, 0] for _ in range(N)] for _ in range(N)]
-pipe_memo[N-1][N-1][0] = 1
-pipe_memo[N-1][N-1][1] = 1
-pipe_memo[N-1][N-1][2] = 1
-write = []
+pipe_memo = [[[-1000, -1000, -1000] for _ in range(N)] for _ in range(N)]
 res = pipe(0, 1, 0)
-print(pipe_memo)
 print(res)
