76f954c2352495907093636f1c68ec51e87860f4
baambox5 <baamboxo@gmail.com>
2019-12-18 23:50:05 +0900

    commit

--- 191216/17070_파이프옮기기_고병권_dp.py
+++ 191216/17070_파이프옮기기_고병권_dp.py
@@ -5,18 +5,16 @@ sys.stdin = open('move_pipe.txt', 'r')
 def pipe(x, y, state):
     if house[x][y] == '1':
         return 0
-    if x == N - 1 and y == N - 1:
-        pipe_memo[x][y][state] = 1
     if pipe_memo[x][y][state] >= 0:
         return pipe_memo[x][y][state]
+    if x == N - 1 and y == N - 1:
+        pipe_memo[x][y][state] = 1
+        return 1
     sum_value = 0
     if state == 0:
         if y < N - 1:
             sum_value += pipe(x, y + 1, 0)
     elif state == 1:
-        if 1 <= x and 1 <= y:
-            if house[x - 1][y] == '1' or house[x][y - 1] == '1':
-                return 0
         if y < N - 1:
             sum_value += pipe(x, y + 1, 0)
         if x < N - 1:
@@ -25,13 +23,14 @@ def pipe(x, y, state):
         if x < N - 1:
             sum_value += pipe(x + 1, y, 2)
     if x < N - 1 and y < N - 1:
-        sum_value += pipe(x + 1, y + 1, 1)
+        if house[x + 1][y] != '1' and house[x][y + 1] != '1':
+            sum_value += pipe(x + 1, y + 1, 1)
     pipe_memo[x][y][state] = sum_value
     return sum_value
 
 
 N = int(input())
 house = [input().split() for _ in range(N)]
-pipe_memo = [[[-1000, -1000, -1000] for _ in range(N)] for _ in range(N)]
+pipe_memo = [[[-1, -1, -1] for _ in range(N)] for _ in range(N)]
 res = pipe(0, 1, 0)
 print(res)
--- 191218/17135_캐슬디펜스_고병권.py
+++ 191218/17135_캐슬디펜스_고병권.py
@@ -1,21 +1,21 @@
 import sys
 sys.stdin = open('castle_defense.txt', 'r')
 from collections import deque
-import copy
 
 
+# 선택된 궁수 3명의 위치로 디펜스 시작
 def defense():
     global max_count, enemy_count
     count = 0
-    copy_position = copy.deepcopy(enemy_position)
+    copy_position = enemy_position.copy()
     enemy = enemy_count
     attack = set()
-    copy_arr = [[0] * M for _ in range(N)]
-    for i in range(N):
-        for j in range(M):
-            copy_arr[i][j] = arr[i][j]
+    copy_arr = [['0'] * M for _ in range(N)]
+    for x, y in copy_position:
+        copy_arr[x][y] = arr[x][y]
+    # 적이 없어질때까지 반복
     while enemy > 0:
-        attack.clear()
+        # 궁수의 위치(성) 바로 앞에서부터 BFS로 사정거리 내에 적이 있는지 확인
         for archer in archers:
             Q = deque()
             visit = {}
@@ -25,33 +25,41 @@ def defense():
                 x, y, d = Q.popleft()
                 if d > D:
                     break
+                # 공격할 적 저장
                 if copy_arr[x][y] == '1':
                     attack.add((x, y))
                     break
                 for dx, dy in delta:
                     nx = x + dx
                     ny = y + dy
-                    if 0 <= nx and 0 <= ny < M and not visit.get((nx, ny)):
+                    if 0 <= nx and 0 <= ny < M:
+                        if visit.get((nx, ny)):
+                            continue
                         Q.append((nx, ny, d + 1))
                         visit[(nx, ny)] = 1
+        # 공격으로 적 없애기
         for x, y in attack:
             copy_arr[x][y] = '0'
             enemy -= 1
             count += 1
-            copy_position.discard((x, y))
+            copy_position.remove((x, y))
+        attack.clear()
         temp = set()
+        # 적들 성 앞으로 한칸씩 이동
         for x, y in copy_position:
             copy_arr[x][y] = '0'
             if x == N - 1:
                 enemy -= 1
             else:
-                copy_arr[x + 1][y] = '1'
                 temp.add((x + 1, y))
-        copy_position = temp
+        for x, y in temp:
+            copy_arr[x][y] = '1'
+        copy_position = temp.copy()
     if max_count < count:
         max_count = count
 
 
+# 조합으로 궁수 3명의 위치 잡기
 def castle(k, s):
     if k == 3:
         defense()
@@ -61,6 +69,7 @@ def castle(k, s):
             castle(k + 1, p + 1)
 
 
+# 입력받기
 N, M, D = map(int, input().split())
 enemy_position = set()
 delta = ((0, -1), (-1, 0), (0, 1))
@@ -68,6 +77,7 @@ arr = []
 enemy_count = 0
 max_count = 0
 archers = [0] * 3
+# 맵 정보 받으면서 적 위치 및 적 숫자 확인
 for i in range(N):
     arr += [input().split()]
     for j in range(M):
